7e77740bc7afe19e3342ac807cef26f0df686451
zod <halozod@outlook.com>
2020-02-20 16:51:32 -0500

    info index update

--- app/views/info/index.php
+++ app/views/info/index.php
@@ -1,9 +1,13 @@
-<div class="row">
-    <div class="col-lg-4" id="1">
+<div class="container">
+    <div class="row card">
+    <div class="col-4 card" id="1">
         <P>here is text</P>
     </div>
-    <div class="col-lg-8" id="2">
+    <div class="col-8 card" id="2">
         <p>here is a pic</p>
     </div>
+    </div>
+    <div class="align-self-end card">
     <p><a href="next">next</p>
+    </div>
 </div>

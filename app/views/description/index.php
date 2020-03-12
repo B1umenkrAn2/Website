642fcbae20b9b0056824f1a83218a98694502dc3
xiongs <halozod@me.com>
2020-03-12 14:33:25 -0400

    selector update

--- app/views/description/index.php
+++ app/views/description/index.php
@@ -2,19 +2,19 @@
 <script>
     $(".talbes").click(function () {
         var tables = $(".tables");
-        if (tables.val()==='Info'){
+        if (tables.val() === 'Info') {
             $("#descTable")
-        }else if (tables.val()==='Chemical'){
+        } else if (tables.val() === 'Chemical') {
 
-        }else if (tables.val()==='Sites'){
+        } else if (tables.val() === 'Sites') {
 
-        }else if (tables.val()==='Profiles'){
+        } else if (tables.val() === 'Profiles') {
 
-        }else if (tables.val()==='Physical'){
+        } else if (tables.val() === 'Physical') {
 
-        }else if (tables.val()==='Morphology'){
+        } else if (tables.val() === 'Morphology') {
 
-        }else if (tables.val()==='Horizons'){
+        } else if (tables.val() === 'Horizons') {
 
         }
 
@@ -24,63 +24,69 @@
 <div class="wrapper">
 
 
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Home</h1>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Home</h1>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="http://localhost">Home</a></li>
+                            <li class="breadcrumb-item active">Home</li>
+                        </ol>
+                    </div>
+                    <!-- /.col -->
                 </div>
-                <!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="http://localhost">Home</a></li>
-                        <li class="breadcrumb-item active">Home</li>
-                    </ol>
-                </div>
-                <!-- /.col -->
+                <!-- /.row -->
             </div>
-            <!-- /.row -->
+            <!-- /.container-fluid -->
         </div>
-        <!-- /.container-fluid -->
+        <!--     /.content-header -->
+
+
+        <!--     Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <h1>Informal description</h1>
+                <p>
+                    A limited dataset of vetted Canadian Soil Information Service holdings for location specific soils
+                    data extracted from Agriculture and Agri-Food Canada’s National Pedon Database (NPDB). The NPDB is a
+                    central repository for storing and disseminating location-specific (pedon) soils data from across
+                    Canada.
+                </p>
+                <div class="row col-12">
+                    <div class="col-4">
+                        <input type="button" value="Chemical" onclick=""/></div>
+                    <div class="col-4">
+                        <input type="button" value="info" onclick=""/></div>
+                    <div class="col-4">
+                        <input type="button" value="info" onclick=""/></div>
+                    <div class="col-4">
+                        <input type="button" value="info" onclick=""/></div>
+                    <div class="col-4">
+                        <input type="button" value="info" onclick=""/></div>
+                    <div class="col-4">
+                        <input type="button" value="info" onclick=""/></div>
+                    <div class="col-4">
+                        <input type="button" value="info" onclick=""/></div>
+                    <div class="col-4">
+                        <input type="button" value="info" onclick=""/></div>
+
+
+                </div>
+                <div class="row col-12">
+                    <table id="descTable">
+
+                    </table>
+                </div>
+            </div>
     </div>
-    <!--     /.content-header -->
-
-
-    <!--     Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <h1 >Description page</h1>
-            <div class="row col-12">
-                <div class="col-4">
-                    <input type="button" value="Chemical"  onclick=""/> </div>
-                <div class="col-4">
-                    <input type="button" value="info"  onclick=""/> </div>
-                <div class="col-4">
-                    <input type="button" value="info"  onclick=""/> </div>
-                <div class="col-4">
-                    <input type="button" value="info"  onclick=""/> </div>
-                <div class="col-4">
-                    <input type="button" value="info"  onclick=""/> </div>
-                <div class="col-4">
-                    <input type="button" value="info"  onclick=""/> </div>
-                <div class="col-4">
-                    <input type="button" value="info"  onclick=""/> </div>
-                <div class="col-4">
-                    <input type="button" value="info"  onclick=""/> </div>
-
-
-                </div>
-            <div class="row col-12"  >
-                <table id="descTable">
-
-                </table>
-            </div>
-            </div>
-        </div>
-        <!-- end of Main content -->
+    <!-- end of Main content -->
 </div>
 
-    <!-- wrapper end at footer file-->
+<!-- wrapper end at footer file-->
